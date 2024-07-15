31b0b249d8d1627812aa332eb67167faafe21b35
Sachin Saxena <sachinsaxena@outlook.in>
2024-07-15 22:57:28 +0530

    Add arm64 assembly code

---
+++ helloarm64.s
@@ -0,0 +1,55 @@
+	.text
+	.file	"hello.c"
+	.globl	main                            // -- Begin function main
+	.p2align	2
+	.type	main,@function
+main:                                   // @main
+	.cfi_startproc
+// %bb.0:
+	sub	sp, sp, #32
+	.cfi_def_cfa_offset 32
+	stp	x29, x30, [sp, #16]             // 16-byte Folded Spill
+	add	x29, sp, #16
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	stur	wzr, [x29, #-4]
+	str	wzr, [sp, #8]
+	b	.LBB0_1
+.LBB0_1:                                // =>This Inner Loop Header: Depth=1
+	ldr	w8, [sp, #8]
+	subs	w8, w8, #10
+	b.ge	.LBB0_4
+	b	.LBB0_2
+.LBB0_2:                                //   in Loop: Header=BB0_1 Depth=1
+	ldr	w1, [sp, #8]
+	adrp	x0, .L.str
+	add	x0, x0, :lo12:.L.str
+	bl	printf
+	b	.LBB0_3
+.LBB0_3:                                //   in Loop: Header=BB0_1 Depth=1
+	ldr	w8, [sp, #8]
+	add	w8, w8, #1
+	str	w8, [sp, #8]
+	b	.LBB0_1
+.LBB0_4:
+	mov	w0, wzr
+	.cfi_def_cfa wsp, 32
+	ldp	x29, x30, [sp, #16]             // 16-byte Folded Reload
+	add	sp, sp, #32
+	.cfi_def_cfa_offset 0
+	.cfi_restore w30
+	.cfi_restore w29
+	ret
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        // -- End function
+	.type	.L.str,@object                  // @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"\nCounter is : %d"
+	.size	.L.str, 17
+
+	.ident	"clang version 18.1.8"
+	.section	".note.GNU-stack","",@progbits
